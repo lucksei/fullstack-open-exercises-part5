abedd7ae6b550850accb1d9745d8251220738719
Luca Seimandi <lseimandi@colsecor.com.ar>
2025-06-28 17:36:43 -0300

    modified ui script for wsl 'ui-wsl' and finished tests for 5.18 -- login successful and failed tests

--- e2e-testing-playwright/tests/blog_app.spec.js
+++ e2e-testing-playwright/tests/blog_app.spec.js
@@ -1,5 +1,5 @@
-// @ts-check
-const { test, expect, beforeEach, describe } = require('@playwright/test');
+const { test, expect, beforeEach, describe, } = require('@playwright/test');
+// @ts-ignore
 const { log } = require('console');
 
 describe('Blog app', () => {
@@ -18,6 +18,40 @@ describe('Blog app', () => {
         const loginPasswordElement = await page.locator('input[name="Password"]')
         await expect(loginPasswordElement).toBeVisible()
         await expect(loginPasswordElement).toHaveAttribute('type', 'password')
+    })
+
+    describe('Login', () => {
+        test('succeeds with correct credentials', async ({ page }) => {
+            const loginUsernameElement = await page.locator('input[name="Username"]')
+            await loginUsernameElement.fill("root")
+
+            const loginPasswordElement = await page.locator('input[name="Password"]')
+            await loginPasswordElement.fill("sekret")
+
+            const loginButtonElement = await page.getByRole('button', { name: 'login' })
+            await loginButtonElement.click()
+
+            await setTimeout(() => { }, 3000)
+
+            const blogsTitleElement = await page.locator('h2', { hasText: 'blogs' })
+            await expect(blogsTitleElement).toBeVisible()
+        })
+
+        test('fails with wrong credentials', async ({ page }) => {
+            const loginUsernameElement = await page.locator('input[name="Username"]')
+            await loginUsernameElement.fill("root")
+
+            const loginPasswordElement = await page.locator('input[name="Password"]')
+            await loginPasswordElement.fill("wrong-password")
+
+            const loginButtonElement = await page.getByRole('button', { name: 'login' })
+            await loginButtonElement.click()
+
+            await setTimeout(() => { }, 3000)
+
+            const blogsTitleElement = await page.locator('h2', { hasText: 'blogs' })
+            await expect(blogsTitleElement).not.toBeAttached()
+        })
     })
 })
 
